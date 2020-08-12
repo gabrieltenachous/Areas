79c25006ab0a3fc94162d423af12153c12e9ef02
gabrieltenachous <gabriel_oliveira_f_sousa@outlook.com>
2020-08-11 22:32:04 -0300

    FirstCommit

---
+++ TestandoAreas/Areas/Produtos/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿ 
+using System.Diagnostics; 
+using Microsoft.AspNetCore.Mvc;
+using TestandoAreas.Models;
+
+namespace TestandoAreas.Areas.Produtos.Controllers
+{
+
+    [Area("Produtos")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description areas.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact areas.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
